c8dd415612876aa20443709910ebd78afa2871f3
Julia Voronetskaja <julia.voronetskaja@tthk.ee>
2021-11-23 09:57:59 +0200

    first commit

---
+++ LoginValidation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LoginValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //login: admin
+            //password: pass1234
+
+            //declare variables
+            //assign value - initialize a variable
+            string login = "admin";
+            string password = "pass1234";
+
+            string userName, userPassword;
+            Console.WriteLine("Enter your login:");
+            userName = Console.ReadLine();
+            Console.WriteLine("Enter your password:");
+            userPassword = Console.ReadLine();
+
+            if(login == userName && password == userPassword)
+            {
+                Console.WriteLine("Welcome!");
+            }
+            else
+            {
+                Console.WriteLine("Oops. Something went wrong.");
+            }
+
+        }
+    }
+}
